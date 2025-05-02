e166ea7eb7d4cdbe90dbf7a87e937846e030a0f4
Yinkajay <yinkajunn@gmail.com>
2025-05-02 10:00:40 +0100

    Fix my links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
         <footer className='absolute h-20 max-w-screen-2xl mx-auto left-0 right-0 bg-aber-300 max-mobile:flex-col bottom-0 p-8 flex justify-center gap-5 max-mobile:gap-2'>
             <ul className='flex gap-3 md:gap-5 max-mobile:mx-auto'>
                 <li>
-                    <Link className='underline'>LinkedIn</Link>
+                    <Link className='underline' to='https://www.linkedin.com/in/yinka-junaid-a9a654171/'>LinkedIn</Link>
                 </li>
                 <li>
-                    <Link className='underline'>Github</Link>
+                    <Link className='underline' to='https://github.com/Yinkajay'>Github</Link>
                 </li>
                 <li>
-                    <Link className='underline'>Twitter</Link>
+                    <Link className='underline' to='https://x.com/zeuslgndd'>Twitter</Link>
                 </li>
                 <li>
                     <Link className='underline' to={'https://adeyinka.hashnode.dev/'}>Hashnode</Link>
