475eb2085b642f30a29eddb783eb839b13c8e598
Yinkajay <yinkajunn@gmail.com>
2025-05-02 10:07:24 +0100

    Add my cv link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -58,7 +58,7 @@ const Home = () => {
                         <li>Git</li>
                     </ul>
                 </div>
-                <Link className='underline' to={''}>LOOK AT MY CV</Link>
+                <Link className='underline' to='https://docs.google.com/document/d/1TOEhoPabfB1uKbiNEBdMuTKcSPwBbhK_2LTiQo95RiQ/edit?tab=t.0'>LOOK AT MY CV</Link>
             </div>
         </motion.div>
     )
